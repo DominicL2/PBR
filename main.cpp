f9b33d47077fd87223a5aad7f57294da4100a5f0
dmlee <dmlee@vadas.co.kr>
2022-04-29 11:56:28 +0900

    WINDOW : apply fixed size of window

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@ int main(int argc, char *argv[])
     qmlRegisterType<ViewModel>("PbrRender", 0, 5, "ViewModel");
 
     QQuickView view;
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.setResizeMode(QQuickView::SizeViewToRootObject);
+    view.setMinimumSize(QSize(1280, 720));
+    view.setMaximumSize(QSize(1280, 720));
+    view.setTitle("DML Graphcis Viewer");
     view.setSource(QUrl("qrc:/main.qml"));
 
     view.show();
